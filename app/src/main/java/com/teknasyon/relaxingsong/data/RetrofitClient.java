3ea3bb62310741d1188c95f13635ceec597380db
anil.kara <>
2018-10-13 22:12:41 +0300

    Basic retrofit client class implementation.

---
+++ app/src/main/java/com/teknasyon/relaxingsong/data/RetrofitClient.java
@@ -0,0 +1,30 @@
+package com.teknasyon.relaxingsong.data;
+
+import android.content.Context;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by anilkara on 13.10.2018.
+ */
+
+public class RetrofitClient {
+
+    private final static String BASE_URL = ""; // will be handled.
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(Context context) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build();
+        }
+        return retrofit;
+    }
+    
+}
